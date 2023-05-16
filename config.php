92c71e81dd2e6b5315583bab6ccdcf6a9de47af9
Alaxdav1d <alaxdavid13@gmail.com>
2023-05-15 21:52:58 -0300

    teste com php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'teste1';
+
+    $conexao = new mysqli($dbHost,$dbUserName,$dbPassword,$dbname);      
+?>
